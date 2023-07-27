7cae462563acd45f407c4e5790995b80c860bff6
MauraPeterson <maurapetersonsoftware@gmail.com>
2023-07-27 16:14:02 -0600

    Play around with react

---
+++ Part 1/index.js
@@ -0,0 +1,11 @@
+function MyButton(){
+  return (
+    <button className="myButton">
+      Click
+    </button>
+  )
+}
+
+ReactDOM.render(<MyButton />, document.getElementById('root'));
+
+// video 29:07
